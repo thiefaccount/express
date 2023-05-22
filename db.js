66e759aa33ffbee30491a93552e5cf9beab1d774
thiefaccount <134274653+thiefaccount@users.noreply.github.com>
2023-05-22 22:54:33 +0545

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connectToMongo = ()=>{
     mongoose.set('strictQuery', false);
     mongoose.connect(mongoURI,()=>{
         console.log("connected to haha mongo successfully");
-        console.log(process.env.mongo_url);
+        console.log("it is the variable" + process.env.mongo_url);
     })
 }
 module.exports = connectToMongo;
